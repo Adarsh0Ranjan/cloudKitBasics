78debbbb40648ec409f39f6337cea9cd4a44c4f5
Adarsh Ranjan <adarsh.ranjan@roro.io>
2025-01-21 05:27:06 +0530

    Initial Commit

---
+++ CloudKitBasics/CloudKitBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitBasicsApp.swift
+//  CloudKitBasics
+//
+//  Created by Adarsh Ranjan on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloudKitBasics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CloudKitBasics
+//
+//  Created by Adarsh Ranjan on 21/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
